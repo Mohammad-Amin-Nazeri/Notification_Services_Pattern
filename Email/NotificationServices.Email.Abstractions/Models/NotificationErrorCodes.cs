ab8c1ecbac76af1d97920a98de4d9a6dfbc10193
Mohammad Amin Nazeri <nazrymhmdamyn85@gmail.com>
2026-08-15 21:20:46 +0330

    feat: add standard notification error codes for email

---
+++ Email/NotificationServices.Email.Abstractions/Models/NotificationErrorCodes.cs
@@ -0,0 +1,9 @@
+namespace NotificationServices.Email.Abstractions.Models;
+
+public static class NotificationErrorCodes
+{
+    public const string InvalidRequest = "InvalidRequest";
+    public const string AuthenticationFailed = "AuthenticationFailed";
+    public const string Timeout = "Timeout";
+    public const string RateLimited = "RateLimited";
+    public
